80974128fe2f07a1f3199cc449a1ae04ba61e2bf
Ryan Hockett <rhockett94@gmail.com>
2017-02-27 01:20:25 +0000

    Re-implemented line drawing functionality

--- Tanks/Line.cs
+++ Tanks/Line.cs
@@ -10,12 +10,18 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Tanks
 {
 	class Line
 	{
-		private LinkedList<Vector2> points = new LinkedList<Vector2>();
+		private LinkedList<Vector2> points;
+
+		public Line()
+		{
+			points = new LinkedList<Vector2>();
+		}
 
 		private void addPoint(Vector2 newPoint)
 		{
@@ -27,5 +33,23 @@ namespace Tanks
 			return points;
 		}
 
+		//http://gamedev.stackexchange.com/a/26027
+		private void DrawLine(SpriteBatch spriteBatch, Texture2D texture, Vector2 start, Vector2 end)
+		{
+			spriteBatch.Draw(texture, start, null, Color.White,
+							 (float)Math.Atan2(end.Y - start.Y, end.X - start.X),
+							 new Vector2(0f, (float)texture.Height / 2),
+							 new Vector2(Vector2.Distance(start, end), 1f),
+							 SpriteEffects.None, 0f);
+		}
+
+		public void drawLines(Texture2D lineTexture, SpriteBatch spriteBatch)
+		{
+			spriteBatch.Begin();
+			DrawLine(spriteBatch, lineTexture, new Vector2(20, 20), new Vector2(120, 120));
+			DrawLine(spriteBatch, lineTexture, new Vector2(120, 20), new Vector2(220, 60));
+			DrawLine(spriteBatch, lineTexture, new Vector2(20, 240), new Vector2(220, 100));
+			spriteBatch.End();
+		}
 	}
 }
